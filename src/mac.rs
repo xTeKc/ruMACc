0b64b2408fc98e991bf808f2953730aeb4dfbd2c
xTeKc <bcd3v@pm.me>
2021-08-17 22:50:24 -0400

    try mac func

--- src/mac.rs
+++ src/mac.rs
@@ -2,3 +2,13 @@ extern crate mac_address;
 
 use mac_address::get_mac_address;
 
+fn mac() {
+    match get_mac_address() {
+        Ok(Some(ma)) => {
+            println!("MAC addr = {}", ma);
+            println!("bytes = {:?}", ma.bytes());
+        }
+        Ok(None) => println!("No MAC address found."),
+        Err(e) => println!("{:?}", e),
+    }
+}
