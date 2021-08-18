2d41e45466ce478a00478db788f9e00eaf86e385
xTeKc <bcd3v@pm.me>
2021-08-17 23:02:29 -0400

    modify symbol

--- src/mac/main_mac.rs
+++ src/mac/main_mac.rs
@@ -5,7 +5,7 @@ use mac_address::get_mac_address;
 pub fn mac() {
     match get_mac_address() {
         Ok(Some(ma)) => {
-            println!("MAC addr = {}", ma);
+            println!("MAC => {}", ma);
         }
         Ok(None) => println!("No MAC address found."),
         Err(e) => println!("{:?}", e),
