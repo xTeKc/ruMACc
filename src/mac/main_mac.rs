d582285bce80a4ad86c32381f230a5154bbe3036
xTeKc <bcd3v@pm.me>
2021-08-18 22:12:48 -0400

    only keep mac addr func

--- src/mac/main_mac.rs
+++ src/mac/main_mac.rs
@@ -5,29 +5,14 @@ use mac_address::{
     mac_address_by_name,
 };
 
-struct Mac {
-    mac_address: String,
-    mac_address_name: String,
+
+
+pub fn mac_addr() {
+    match get_mac_address() {
+        Ok(Some(ma)) => {
+            println!("MAC Address -> {}", ma);
+        }
+        Ok(None) => println!("No MAC Address found."),
+        Err(e) => println!("{:?}", e),
+    }
 }
-
-
-// pub fn mac_addr() {
-//     match get_mac_address() {
-//         Ok(Some(ma)) => {
-//             println!("MAC Address -> {}", ma);
-//         }
-//         Ok(None) => println!("No MAC Address found."),
-//         Err(e) => println!("{:?}", e),
-//     }
-// }
-
-
-// pub fn mac_name() {
-//     match mac_address_by_name(name) {
-//         Ok(Some(ma)) => {
-//             println!("MAC Address of {} = {}", name, ma);
-//         }
-//         Ok(None) => println!("Interface \"{}\" not found", name),
-//         Err(e) => println!("{:?}", e),
-//     }
-// }
